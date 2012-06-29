fa4f597111dfdf494bf322c785b4a7b0d9cd3021
Kelvin Hu <ini.kelvin@gmail.com>
2012-06-29 21:06:37 +0800

    changed a theme

--- conf/conf-theme.el
+++ conf/conf-theme.el
@@ -2,7 +2,7 @@
 ; ******* theme config *******
 ;        Author:  Kelvin Hu
 ;       Created:  05/07/2012
-; Last Modified:  06/02/2012
+; Last Modified:  06/29/2012
 ;---------------------------------------------------------------------------------
 
 ;---------------------------------------------------------------------------------
@@ -11,6 +11,7 @@
 (add-to-list 'load-path "~/.emacs.d/lib/color-theme")
 (add-to-list 'load-path "~/.emacs.d/lib/color-theme/solarized")
 (add-to-list 'load-path "~/.emacs.d/lib/color-theme/molokai")
+(add-to-list 'load-path "~/.emacs.d/lib/color-theme/sanityinc-solarized")
 
 ;(require 'color-theme)
 ;(color-theme-initialize)
@@ -19,9 +20,11 @@
 ;(require 'color-theme-molokai)
 ;(color-theme-molokai)
 
-(require 'color-theme-solarized)
-(color-theme-solarized 'dark)
+;(require 'color-theme-solarized)
+;(color-theme-solarized 'dark)
 
+(require 'color-theme-sanityinc-solarized)
+(color-theme-sanityinc-solarized 'dark)
 
 
 (provide 'conf-theme)
