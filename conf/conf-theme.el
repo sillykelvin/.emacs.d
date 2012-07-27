2e6c71f5aca3ad2bebf3078710d78436a87b27f8
Kelvin Hu <ini.kelvin@gmail.com>
2012-07-27 22:07:10 +0800

    changed color theme to solarized, and updated .gitignore

--- conf/conf-theme.el
+++ conf/conf-theme.el
@@ -2,7 +2,7 @@
 ; ******* theme config *******
 ;        Author:  Kelvin Hu
 ;       Created:  05/07/2012
-; Last Modified:  07/07/2012
+; Last Modified:  07/26/2012
 ;---------------------------------------------------------------------------------
 
 ;---------------------------------------------------------------------------------
@@ -22,8 +22,8 @@
 ;(require 'color-theme-molokai)
 ;(color-theme-molokai)
 
-;(require 'color-theme-solarized)
-;(color-theme-solarized 'dark)
+(require 'color-theme-solarized)
+(color-theme-solarized 'dark)
 
 ;(require 'color-theme-sanityinc-solarized)
 ;(color-theme-sanityinc-solarized 'dark)
@@ -31,8 +31,8 @@
 ;(require 'color-theme-tomorrow)
 ;(color-theme-tomorrow-real 'night)
 
-(require 'color-theme-sanityinc-tomorrow)
-(color-theme-sanityinc-tomorrow 'night)
+;(require 'color-theme-sanityinc-tomorrow)
+;(color-theme-sanityinc-tomorrow 'night)
 
 ;;; set special font for Chinese
 (dolist (charset '(kana han symbol cjk-misc bopomofo))
