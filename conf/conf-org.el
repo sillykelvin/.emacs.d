2b9a52f6f90fe59297e5fbecbe4b5acc8007eca4
Kelvin Hu <ini.kelvin@gmail.com>
2012-12-14 20:19:59 +0800

    improved the hook definition

--- conf/conf-org.el
+++ conf/conf-org.el
@@ -2,19 +2,16 @@
 ; ******* org config *******
 ;        Author:  Kelvin Hu
 ;       Created:  07/29/2012
-; Last Modified:  11/30/2012
+; Last Modified:  12/14/2012
 ;---------------------------------------------------------------------------------
-
 
 (require 'util-org)
 
-(add-hook 'org-mode-hook
-          (lambda ()
-            (org-defkey org-mode-map "\C-ct" 'kh/org-insert-export-options-template)))
-
-(add-hook 'org-mode-hook
-          (lambda ()
-            (setq truncate-lines t)))
+(kh/add-hook 'org-mode-hook
+             '((lambda ()
+                 (org-defkey org-mode-map "\C-ct" 'kh/org-insert-export-options-template))
+               (lambda ()
+                 (setq truncate-lines t))))
 
 
 (provide 'conf-org)
