a5cd6183acd160422e0d3416f3aa2f75b3bc9b26
Kelvin Hu <ini.kelvin@gmail.com>
2013-03-27 10:55:42 +0800

    update configuration of dos mode

--- conf/conf-programming.el
+++ conf/conf-programming.el
@@ -1,13 +1,14 @@
-;---------------------------------------------------------------------------------
+;-------------------------------------------------------------------------------
 ; ******* main programming config *******
 ;        Author:  Kelvin Hu
 ;       Created:  06/16/2012
-; Last Modified:  12/12/2012
-;---------------------------------------------------------------------------------
+; Last Modified:  03/27/2013
+;-------------------------------------------------------------------------------
 
 (add-to-list 'load-path "~/.emacs.d/conf/programming")
 
-(require 'conf-batch)
+;; do not require this configuration as dos.el cannot be found in elpa or melpa
+;; (require 'conf-batch)
 
 (require 'conf-lisp)
 
