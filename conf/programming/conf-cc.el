a025f5a1d5c61bd13ea8c3a7263af916e2400b5e
Kelvin Hu <ini.kelvin@gmail.com>
2013-03-28 15:14:11 +0800

    new configuration for cc mode

---
+++ conf/programming/conf-cc.el
@@ -0,0 +1,45 @@
+;-------------------------------------------------------------------------------
+; ******* cc mode config *******
+;        Author:  Kelvin Hu
+;       Created:  03/28/2013
+; Last Modified:  03/28/2013
+;-------------------------------------------------------------------------------
+
+;; this statement must be put before the next require statement, otherwise the
+;; program clang.exe will not be found while doing auto completion.
+(when is-os-windows
+  (setq exec-path (cons (expand-file-name "D:/SDTs/Clang/") exec-path)))
+
+(require 'auto-complete-clang)
+
+(setq ac-clang-flags
+      (if is-os-windows
+          (list "-ID:/SDTs/MinGW/include/"
+                "-ID:/SDTs/MinGW/lib/gcc/mingw32/4.7.2/include/"
+                "-ID:/SDTs/MinGW/lib/gcc/mingw32/4.7.2/include/c++/"
+                "-ID:/SDTs/MinGW/lib/gcc/mingw32/4.7.2/include/c++/mingw32/"
+                "-D__MSVCRT__")
+        (list "-I/usr/include/c++/4.7.2/"
+              "-I/usr/include/"
+              "-I/usr/include/c++/4.7.2/x86_64-unknown-linux-gnu/")))
+
+(defun kh/ac-cc-mode-setup ()
+  "This function is an extended version of `ac-cc-mode-setup'."
+  ;; (make-local-variable 'ac-auto-start)
+  ;; (setq ac-auto-start nil)
+  (setq ac-sources (append '(ac-source-yasnippet ac-source-clang) ac-sources))
+  (setq c-basic-offset 4)
+  (setq c-indent-level 4)
+  (setq indent-tabs-mode nil)
+  ;; minor modes
+  ;; (auto-fill-mode 1)
+  ;; (c-turn-on-eldoc-mode)
+  ;; (gtags-mode 1)
+  ;; (hs-minor-mode 1)
+  ;; (define-key c-mode-base-map [(meta ?/)] 'semantic-ia-complete-symbol-menu)
+  )
+(add-hook 'c-mode-common-hook 'kh/ac-cc-mode-setup)
+;(define-key ac-mode-map (kbd "M-/") 'auto-complete)
+
+
+(provide 'conf-cc)
