9052b1e95527c80726c7227fda5ae146d3a4ded8
Kelvin Hu <ini.kelvin@gmail.com>
2013-03-28 15:16:33 +0800

    update configuration of yasnippet

--- conf/conf-yasnippet.el
+++ conf/conf-yasnippet.el
@@ -2,14 +2,12 @@
 ; ******* yasnippet config *******
 ;        Author:  Kelvin Hu
 ;       Created:  07/29/2012
-; Last Modified:  07/29/2012
+; Last Modified:  03/28/2013
 ;---------------------------------------------------------------------------------
-
-(add-to-list 'load-path "~/.emacs.d/lib/yasnippet")
 
 (require 'yasnippet)
 
-(yas/global-mode 1)
+(yas-global-mode 1)
 
 
 (provide 'conf-yasnippet)
