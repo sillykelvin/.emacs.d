7a9f2046985ed727ea2fc50ffa633960fb2560d3
Kelvin Hu <ini.kelvin@gmail.com>
2013-07-21 13:51:13 +0800

    update conf-lisp.el: add configuration for scheme

--- conf/programming/conf-lisp.el
+++ conf/programming/conf-lisp.el
@@ -1,15 +1,19 @@
-;---------------------------------------------------------------------------------
+;-------------------------------------------------------------------------------
 ; ******* Lisp style programming config *******
 ;        Author:  Kelvin Hu
 ;       Created:  12/12/2012
-; Last Modified:  12/14/2012
-;---------------------------------------------------------------------------------
+; Last Modified:  07/21/2013
+;-------------------------------------------------------------------------------
 
 (require 'util-common)
 
 (kh/add-hook '(lisp-mode-hook emacs-lisp-mode-hook)
              '((lambda ()
                 (show-paren-mode t))))
+
+(if is-os-windows
+    (setq scheme-program-name "racket")
+  (setq scheme-program-name "guile"))
 
 
 (provide 'conf-lisp)
