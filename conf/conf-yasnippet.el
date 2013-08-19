0bfd8c31435718b184e28ab54a3cb881b4a0b3fc
Kelvin Hu <ini.kelvin@gmail.com>
2013-08-19 15:00:36 +0800

    update yasnippet configuration

--- conf/conf-yasnippet.el
+++ conf/conf-yasnippet.el
@@ -1,13 +1,22 @@
-;---------------------------------------------------------------------------------
+;-------------------------------------------------------------------------------
 ; ******* yasnippet config *******
 ;        Author:  Kelvin Hu
 ;       Created:  07/29/2012
-; Last Modified:  03/28/2013
-;---------------------------------------------------------------------------------
+; Last Modified:  08/19/2013
+;-------------------------------------------------------------------------------
 
 (require 'yasnippet)
 
 (yas-global-mode 1)
+
+;;; I copied the configuration from:
+;;;   https://bitbucket.org/Fuco/.emacs.d/.../files/yasnippet.el
+;;; I don't know if this helps...
+(defun kh/yas-startup ()
+  (define-key yas-minor-mode-map [(tab)] nil)
+  (define-key yas-minor-mode-map (kbd "TAB") nil))
+
+(add-hook 'yas-minor-mode-hook 'kh/yas-startup)
 
 
 (provide 'conf-yasnippet)
